30f4b5a79fd834481c0cd9b7ac3a1ce66d184194
vianneyKB <36236520+vianneyKB@users.noreply.github.com>
2022-11-26 14:45:50 +0200

    pack update

--- backend/src/auth/utils.ts
+++ backend/src/auth/utils.ts
@@ -12,18 +12,18 @@ export function parseUserId(jwtToken: string): string {
   return decodedJwt.sub 
 }
 
-// export function getToken(authHeader: string): string {
+export function getToken(authHeader: string): string {
 
-//   if (!authHeader) {
-//     throw new Error('No authentication header');
-//   }
+  if (!authHeader) {
+    throw new Error('No authentication header');
+  }
 
-//   if (!authHeader.toLowerCase().startsWith('bearer ')) {
-//     throw new Error('Invalid authentication header');
-//   }
+  if (!authHeader.toLowerCase().startsWith('bearer ')) {
+    throw new Error('Invalid authentication header');
+  }
 
-//   // const split = authHeader.split(' ');
-//   const token = authHeader
+  const split = authHeader.split(' ');
+  const token = split[1];
 
-//   return token;
-// }
+  return token;
+}
