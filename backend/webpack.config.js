8d43b875ff3e6bdb561bfd5b5758e4d02a82a239
vianneyKB <36236520+vianneyKB@users.noreply.github.com>
2022-11-27 13:32:55 +0200

    crud OP done

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,29 +1,21 @@
 const path = require('path');
 const slsw = require('serverless-webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 // var nodeExternals = require('webpack-node-externals')
 
 module.exports = {
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   entry: slsw.lib.entries,
   // externals: [nodeExternals()],
-  // entry: 'index.tsx',
   devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
   },
-  
   output: {
     libraryTarget: 'commonjs',
-    path: path.join(__dirname + '/dist', '.webpack'),
+    path: path.join(__dirname, '.webpack'),
     filename: '[name].js',
-    // path: __dirname + '/dist',
-    // publicPath: '/',
-    // filename: 'index_bundle.js'
   },
-  plugins: [
-    new HtmlWebpackPlugin()
-  ],
   target: 'node',
   module: {
     rules: [
